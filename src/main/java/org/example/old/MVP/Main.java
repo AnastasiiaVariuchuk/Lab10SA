d1790d3184f80be3379bce5a302df97e6ff42e9e
Anastasiia Variichuk <stacyvariuchuk@gmail.com>
2023-12-07 21:52:58 +0200

    update

--- src/main/java/org/example/old/MVP/Main.java
+++
@@ -1,8 +0,0 @@
-package org.example.old.MVP;
-
-public class Main {
-    public static void main(String[] args) {
-        TriangularPrismView view = new TriangularPrismView();
-        view.requestCalculation();
-    }
-}
